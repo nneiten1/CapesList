4de53a58a58994e2a3a9f8669a6cd6696da6d88c
Cornelius Treadway Pavlic <cornelius.pavlic@gmail.com>
2024-03-06 09:54:25 -0500

    Rest of files

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+
+const HeaderDiv = styled.div`
+    width:100%;
+    height:100px;
+    background-color: cornflowerblue;
+`
+function Header() {
+  return (
+   <HeaderDiv>
+
+
+   </HeaderDiv>
+  );
+}
+
+export default Header;
