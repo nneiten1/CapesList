d85b66ee0042fa761e024ac46baba164ff7e5e30
Austin Joseph <austin@Austins-MacBook-Pro.local>
2024-05-07 13:18:20 -0400

    review style changes

--- Frontend/src/components/Reviewitem.js
+++ Frontend/src/components/Reviewitem.js
@@ -1,12 +1,18 @@
+import styled from "styled-components"
+
+const ReviewDiv = styled.div`
+background-color: #121212
+`
+
+
 function Review ({review}) {
     return (
-        <div>
-          <h2 style={{color: 'White'}}>{review.REVIEWER_USER_ID}</h2>
-          <p>{review.REVIEWIE_USER_ID}</p>
+        <ReviewDiv>
+          <h2 style={{color: 'Blue'}}>{review.REVIEWIE_USER_ID}</h2>
           <p>{review.STAR_RATING}</p>
           <p>{review.DATE}</p>
           <p>{review.DETAILS}</p>
-        </div>
+        </ReviewDiv>
       );
 };
 
