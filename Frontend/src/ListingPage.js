fc03aee99119cfca9c0e4f4036f2fc01cbe7ae12
Nick Neitenbach <nneiten1@kent.edu>
2024-05-08 19:52:52 -0400

    ListingPage styling

--- Frontend/src/ListingPage.js
+++ Frontend/src/ListingPage.js
@@ -3,34 +3,27 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import NavBar from "./components/NavBar";
 import Listing from "./components/ListingItem";
-  
-
-
 
 function ListingPage() {
   const { listingID } = useParams();
   const [post, updatePost] =  useState(["Hello"]);
 
-
-
   useEffect(() => {
-  axios.get(`http://localhost:80/Listings/${listingID}` )
-  .then ((response) => {
-    updatePost(response.data);
-  });
+    axios.get(`http://localhost:80/Listings/${listingID}` )
+      .then ((response) => {
+        updatePost(response.data);
+      });
   },[]);
 
-
   return (
-    
     <>
-      <NavBar></NavBar>
-      <Listing key={post.id} listing={post[0]} />
+      <NavBar />
+      <div style={{ color: "white", margin: "20px auto", maxWidth: "800px" }}>
+        <Listing key={post.id} listing={post[0]} />
+      </div>
       <a href={`/user/reviews/${post[0].SELLER_USER_ID}`}> Seller Reviews</a>
     </>
-  )
-    
-};
-
+  );
+}
 
 export default ListingPage;
